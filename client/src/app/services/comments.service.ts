fc56e0fd49dbeca0a081743851c1bd91f02532f5
dragonINXS <davidmudd@outlook.com>
2018-08-10 14:25:37 -0400

    delpy stuff

--- client/src/app/services/comments.service.ts
+++ client/src/app/services/comments.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import { Observable, of } from 'rxjs';
-// import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map'
 import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
